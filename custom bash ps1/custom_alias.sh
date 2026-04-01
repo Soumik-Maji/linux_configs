6e7352a6731ff6ce8c1bf57e7778c6bf07fda046
Otter Kun <88390194+Soumik-Maji@users.noreply.github.com>
2026-04-01 08:06:12 +0530

    alias update

--- custom bash ps1/custom_alias.sh
+++ custom bash ps1/custom_alias.sh
@@ -1,3 +1,9 @@
 alias kdiff='kitten diff'
-alias offhotspot='nmcli connection down hotspot && nmcli radio wifi off'
-alias onhotspot='nmcli radio wifi on && nmcli connection up hotspot'
+
+alias onwifihotspot='nmcli connection up hotspot'
+alias offwifihotspot='nmcli connection down hotspot'
+
+alias offhotspot='offwifihotspot && nmcli radio wifi off'
+alias onhotspot='nmcli radio wifi on && onwifihotspot'
+
+alias update='sudo apt --fix-broken install && sudo apt update && sudo apt upgrade -y && flatpak update -y && sudo apt autopurge -y && sudo apt autoclean && sudo journalctl --vacuum-time=2weeks'
